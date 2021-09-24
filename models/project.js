979f43af09c60f80cde418f2a9db5ce0776256c6
Ryan Crowder <crowderyan@gmail.com>
2021-09-23 20:57:27 -0400

    added description

--- models/project.js
+++ models/project.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
+  projectid: { type: Number, required: true },
   projectname: { type: String, required: true },
   projectowner: [{ type: String, required: true }],
   projectlink: { type: String, trim: true },
+  description: { type: String },
   creationdate: { type: Date, default: Date.now },
   deadline: { type: Date, required: true },
 });
