461a82d0825d25145652961683babae0a8414ad6
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-24 20:22:44 -0400

    created one model file combining user, project, and comment.

---
+++ models/oneModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    github: { type: String },
+    projects: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project"
+    }]
+});
+
+const ProjectSchema = new Schema({
+    title: { type: String, required: true },
+    project_owner: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    github_repo: { type: String, trim: true },
+    description: { type: String },
+    creationdate: { type: Date, default: Date.now },
+    deadline: { type: Date, required: true },
+    role_needed: { type: String },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    }]
+});
+
+const CommentSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project"
+    },
+    body: { type: String, required: true } 
+});
+
+const User = mongoose.model("User", UserSchema);
+const Project = mongoose.model("Project", ProjectSchema);
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = {User, Project, Comment};
