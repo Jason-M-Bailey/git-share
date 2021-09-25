32fdb4373ae829c0166a163e706c0dc18988673b
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-25 08:52:58 -0400

    defining usersController methods

---
+++ controllers/usersController.js
@@ -0,0 +1,23 @@
+const db = require("../models/oneModel");
+
+module.exports = {
+    findAll: function(req, res) {
+        db.User
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(400).json(err));
+    },
+    findOne: function (req, res) {
+        db.User
+            .findOne()
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(400).json(err));
+    },
+    create: function(req, res) {
+        db.User
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(400).json(err));
+    }
+};
