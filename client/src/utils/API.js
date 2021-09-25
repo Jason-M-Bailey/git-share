5f7b1d59d35aa114524b7d2824eb59acc21aa6bd
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-25 10:55:52 -0400

    modified .gitignore; added methods to API util; testing

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,4 +5,24 @@ export default {
   getGithubUser: function (query) {
     return axios.get("https://api.github.com/" + query);
   },
+
+  // get users from database
+  getUsers: function() {
+    return axios.get("/api/users");
+  },
+
+  // save user to database
+  saveUser: function(userData) {
+    return axios.post("/api/users", userData);
+  },
+
+  // get projects from database
+  getProjects: function() {
+    return axios.get("/api/projects");
+  },
+
+  // save project to database
+  saveProject: function(projectData) {
+    return axios.post("/api/projects", projectData);
+  },
 };
