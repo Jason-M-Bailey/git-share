fa2eeeeee5b7fa6171d6d71f08a4ec4cac2bb3e0
Jason Bailey <jmbailey816@gmail.com>
2021-09-28 19:41:53 -0400

    Card layout design

--- client/src/components/ProjectCard/projectcard.js
+++ client/src/components/ProjectCard/projectcard.js
@@ -1,75 +1,55 @@
-
-// import React from "react";
+// import "./projectcard.css";
+import { Card } from "react-bootstrap";
+import React from "react";
 // import Card from "react-bootstrap/Card";
 
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-// import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
-// function Project() {
-//   return (
-//     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-//       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-//         <div className="flex flex-col w-full mb-20">
-          
-//           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-//             Why does the text start right at the top, being hidden behind the navbar? Need to increase the padding or something. 
-//           </p>
-//         </div>
-//         <Row xs={1} md={2} lg={3} xl={4} className="g-4">
-//           {Array.from({ length: 12 }).map((_, idx) => (
-//             <Col>
-//               <Card>
-//                 <Card.Img
-//                   variant="top"
-//                   src="https://github.githubassets.com/images/modules/site/social-cards/github-social.png"
-//                 />
-//                 <Card.Body>
-//                   <Card.Title>Card title</Card.Title>
-//                   <Card.Text>
-//                     With supporting text below as a natural lead-in to
-//                     additional content.
-//                   </Card.Text>
-//                   {/* <Button variant="primary">CSS</Button>
-//                   <Button variant="primary">JavaScript</Button>
-//                   <Button variant="primary">React</Button> */}
-//                   <Card.Link href="#">Card Link</Card.Link>
-//                   <Card.Link href="#">Another Link</Card.Link>
-//                 </Card.Body>
-//                 <Button variant="primary">Go somewhere</Button>
-//                 <Card.Footer className="text-muted">2 days ago</Card.Footer>
-//               </Card>
-//             </Col>
-//           ))}
-//         </Row>
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default Project;
-
-import "./projectcard.css";
-import { Card } from "react-bootstrap";
 
 function ProjectCard(props) {
   return (
-    <div class="ui link cards">
-      {props.projects.map((seed) => {
-        return (
-          <div classname="card">
-            <Card.Body>
-              <Card.Title>{seed.name}</Card.Title>
-              <Card.Title>{seed.repo}</Card.Title>
-              <Card.Text>{seed.description}</Card.Text>
-              <Card.Text>{seed.role_needed} : Needed</Card.Text>
-            </Card.Body>
-          </div>
-        );
-      })}
-    </div>
+    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-20">
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            Why does the text start right at the top, being hidden behind the
+            navbar? Need to increase the padding or something. Why does the text
+            start right at the top, being hidden behind the navbar? Need to
+            increase the padding or something. Why does the text start right at
+            the top, being hidden behind the navbar? Need to increase the
+            padding or something. Why does the text start right at the top,
+            being hidden behind the navbar? Need to increase the padding or
+            something.
+          </p>
+        </div>
+
+        {/* <div class="ui link cards"> */}
+
+        <Row xs={1} md={2} lg={3} xl={4} className="g-4">
+          {props.projects.map((seed) => {
+            return (
+              <div classname="card">
+                <Card border="dark" style={{ width: '18rem' }}>
+                  <Card.Body>
+                  <Card.Header as="h5">{seed.name}</Card.Header>
+                    {/* <Card.Title>{seed.name}</Card.Title> */}
+                    <Card.Title>{seed.repo}</Card.Title>
+                    <Card.Text>{seed.description}</Card.Text>
+                    <Card.Text>{seed.role_needed}</Card.Text>
+                    <Card.Text className="text-muted">Comments: {seed.comments} | Bookmarks: {seed.bookmarks}</Card.Text>
+                    <Card.Footer className="text-muted" >Project Created: {seed.project_created} </Card.Footer>
+                  </Card.Body>
+                </Card>
+              </div>
+            );
+          })}
+        </Row>
+        {/* </div> */}
+      </div>
+    </section>
   );
 }
 
 export default ProjectCard;
-
