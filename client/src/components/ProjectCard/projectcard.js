53b194a8034be2ca36ba380ee653697cc24fd852
Jason Bailey <jmbailey816@gmail.com>
2021-09-28 21:30:11 -0400

    added link to github repo to cards

--- client/src/components/ProjectCard/projectcard.js
+++ client/src/components/ProjectCard/projectcard.js
@@ -1,8 +1,6 @@
-// import "./projectcard.css";
 import { Card } from "react-bootstrap";
 import React from "react";
 import { Wrapper } from "./ProjectCard.styles";
-// import Card from "react-bootstrap/Card";
 
 import Row from "react-bootstrap/Row";
 
@@ -10,30 +8,24 @@ function ProjectCard(props) {
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-        {/* <div className="flex flex-col w-full mb-20"> */}
-          <Wrapper />
-
-        {/* </div> */}
-
-        {/* <div class="ui link cards"> */}
-
+        <Wrapper />
         <Row xs={1} md={2} lg={3} xxl={4} className="g-4">
           {props.projects.map((seed) => {
             return (
               <div classname="card">
                 <Card border="dark" style={{ width: "18rem" }}>
-
-                  {/* add to seed data if we want images on the cards */}
-                {/* <Card.Img variant="top" src={seed.image} /> */}
                   <Card.Body>
                     <Card.Header as="h5">{seed.name}</Card.Header>
-                    {/* <Card.Title>{seed.name}</Card.Title> */}
                     <Card.Title>{seed.repo}</Card.Title>
                     <Card.Text>{seed.description}</Card.Text>
                     <Card.Text>{seed.role_needed}</Card.Text>
+                    <Card.Text>
+                      <a href={seed.github_repo}>Visit Github Repo</a>
+                    </Card.Text>
                     <Card.Text className="text-muted">
                       Comments: {seed.comments} | Bookmarks: {seed.bookmarks}
                     </Card.Text>
+
                     <Card.Footer className="text-muted">
                       Project Created: {seed.project_created}
                     </Card.Footer>
@@ -43,7 +35,6 @@ function ProjectCard(props) {
             );
           })}
         </Row>
-        {/* </div> */}
       </div>
     </section>
   );
