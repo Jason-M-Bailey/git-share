6d81fad3f859b72ee723889664465ce9e3f5e1d0
Jason Bailey <jmbailey816@gmail.com>
2021-09-28 20:54:23 -0400

    Added comments, bookmarks, project created to all seed. Updated route for home, placeholder for search so we know it searches github repos rather than seed. Padding on home page for visual improvement

--- client/src/components/ProjectCard/projectcard.js
+++ client/src/components/ProjectCard/projectcard.js
@@ -1,28 +1,19 @@
 // import "./projectcard.css";
 import { Card } from "react-bootstrap";
 import React from "react";
+import { Wrapper } from "./ProjectCard.styles";
 // import Card from "react-bootstrap/Card";
 
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 
 function ProjectCard(props) {
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-        <div className="flex flex-col w-full mb-20">
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Why does the text start right at the top, being hidden behind the
-            navbar? Need to increase the padding or something. Why does the text
-            start right at the top, being hidden behind the navbar? Need to
-            increase the padding or something. Why does the text start right at
-            the top, being hidden behind the navbar? Need to increase the
-            padding or something. Why does the text start right at the top,
-            being hidden behind the navbar? Need to increase the padding or
-            something.
-          </p>
-        </div>
+        {/* <div className="flex flex-col w-full mb-20"> */}
+          <Wrapper />
+
+        {/* </div> */}
 
         {/* <div class="ui link cards"> */}
 
@@ -31,6 +22,9 @@ function ProjectCard(props) {
             return (
               <div classname="card">
                 <Card border="dark" style={{ width: "18rem" }}>
+
+                  {/* add to seed data if we want images on the cards */}
+                {/* <Card.Img variant="top" src={seed.image} /> */}
                   <Card.Body>
                     <Card.Header as="h5">{seed.name}</Card.Header>
                     {/* <Card.Title>{seed.name}</Card.Title> */}
