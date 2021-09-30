75e362e88705ffb5f6db1adb046439113a06d7ae
Ryan Crowder <crowderyan@gmail.com>
2021-09-30 07:19:29 -0400

    modifications for page layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar/navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import ProjectCard from "./components/ProjectCard/projectcard";
 
-import ProjectCard from "./components/ProjectCard/projectcard";
 import projectseed from "./seed/projectData.json";
 import React, { useState } from "react";
 import API from "./utils/API";
-
+import ProjectCreate from "./pages/newproject";
+import FrontPage from "./pages/frontpage";
 
 function App() {
   const [projects, setProjects] = useState(projectseed);
@@ -28,16 +30,20 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar
+        handleInputSearch={handleInputSearch}
+        setRepos={setRepos}
+        repos={repos}
+      />
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={FrontPage} projects={projects} />
 
-      <div>
-        <Navbar
-          handleInputSearch={handleInputSearch}
-          setRepos={setRepos}
-          repos={repos}
-        />
-        <ProjectCard projects={projects} />
-      </div>
-
+            <Route exact path="/projectcreate" component={ProjectCreate} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
