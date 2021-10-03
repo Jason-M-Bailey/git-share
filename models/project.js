e06f57e49c926538774235479bb9abdd27b6acda
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-10-03 18:49:17 -0400

    updated Project model

--- models/project.js
+++ models/project.js
@@ -1,17 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const projectSchema = new Schema({
-  // projectid: { type: Number, required: true },
-  name: { type: String, required: true },
-  // projectowner: [{ type: String, required: true }],
+const ProjectSchema = new Schema({
+  id: { type: String, required: true },
+  title: { type: String, required: true },
+  project_owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
   github_repo: { type: String, trim: true },
   description: { type: String },
   creationdate: { type: Date, default: Date.now },
   deadline: { type: Date, required: true },
   role_needed: { type: String },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment"
+  }]
 });
 
-const Project = mongoose.model("project", projectSchema);
+const Project = mongoose.model("Project", ProjectSchema);
 
 module.exports = Project;
