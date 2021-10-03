9591e1d0a92299762f8f9eadb4d76ede5ef45a38
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-10-03 18:43:49 -0400

    created separate comment model

---
+++ models/comment.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    id: { type: String, required: true, unique: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project"
+    },
+    body: { type: String, required: true } 
+});
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
