adbfbbbee509922e42fdc375421cad2597fdee45
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-10-03 18:47:31 -0400

    updated User model

--- models/user.js
+++ models/user.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  id: { type: Number, required: true },
+const UserSchema = new Schema({
+  id: { type: String, required: true },
   name: { type: String, required: true },
-  password: { type: String, required: true },
   email: { type: String, required: true },
+  password: { type: String, required: true },
   github: { type: String },
+  projects: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Project"
+  }]
 });
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
