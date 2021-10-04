10b86cd1a55ef2807c8676b1103a5729b93db926
Jason Bailey <jmbailey816@gmail.com>
2021-10-04 14:27:03 -0400

    Deleting unnecessary files, tested and still functional locally

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,73 +1,73 @@
-import React from "react";
+// import React from "react";
 
-// todo: is there a standard way to import these?
-import { Accordion, Card, Row } from "react-bootstrap";
-// import Row from "react-bootstrap/Row";
-import Stack from "react-bootstrap/Stack";
+// // todo: is there a standard way to import these?
+// import { Accordion, Card, Row } from "react-bootstrap";
+// // import Row from "react-bootstrap/Row";
+// import Stack from "react-bootstrap/Stack";
 
-import { Wrapper } from "./Layout.styles";
+// import { Wrapper } from "./Layout.styles";
 
-function ProjectCard(props) {
-  return (
-    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-        <Wrapper />
-        <Row xs={12} md={12} lg={12} xxl={12} className="g-4">
-          <div classname="card">
-            <Card border="dark">
-              <Card.Body>
-                <Card.Header as="h1">NBA Player Search</Card.Header>
+// function ProjectCard(props) {
+//   return (
+//     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+//       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+//         <Wrapper />
+//         <Row xs={12} md={12} lg={12} xxl={12} className="g-4">
+//           <div classname="card">
+//             <Card border="dark">
+//               <Card.Body>
+//                 <Card.Header as="h1">NBA Player Search</Card.Header>
 
-                <Card.Text>
-                  An app to look up nba players, learn more about their stats
-                  and history. And most importantly, to know more than your
-                  friends so you can win those arguments about whether LeBron is
-                  better than Kobe.
-                </Card.Text>
+//                 <Card.Text>
+//                   An app to look up nba players, learn more about their stats
+//                   and history. And most importantly, to know more than your
+//                   friends so you can win those arguments about whether LeBron is
+//                   better than Kobe.
+//                 </Card.Text>
 
-                <Stack gap={3}>
-                  <div className="bg-light border">
-                    ToDo First Item ***Draggable*** for Project Owner?
-                  </div>
-                  <div className="bg-light border">
-                    ToDo Second Item ***Draggable*** for Project Owner?
-                  </div>
-                  <div className="bg-light border">
-                    ToDo Third Item ***Draggable*** for Project Owner?
-                  </div>
-                </Stack>
+//                 <Stack gap={3}>
+//                   <div className="bg-light border">
+//                     ToDo First Item ***Draggable*** for Project Owner?
+//                   </div>
+//                   <div className="bg-light border">
+//                     ToDo Second Item ***Draggable*** for Project Owner?
+//                   </div>
+//                   <div className="bg-light border">
+//                     ToDo Third Item ***Draggable*** for Project Owner?
+//                   </div>
+//                 </Stack>
 
                 
-                <Card.Text className="text-muted">
-                  Comments: 2 | Bookmarks: 4
-                </Card.Text>
+//                 <Card.Text className="text-muted">
+//                   Comments: 2 | Bookmarks: 4
+//                 </Card.Text>
 
-                <Accordion defaultActiveKey="0">
-                  <Accordion.Item eventKey="0">
-                    <Accordion.Header>View Comments</Accordion.Header>
-                    <Accordion.Body >
-                      <Card.Text>
-                        user: this is a great project but could use some UX
-                        improvements!
-                      </Card.Text>
-                      <Card.Text>user: can I join your team?</Card.Text>
-                      <Card.Text>user: neato!</Card.Text>
-                    </Accordion.Body>
-                  </Accordion.Item>
-                </Accordion>
+//                 <Accordion defaultActiveKey="0">
+//                   <Accordion.Item eventKey="0">
+//                     <Accordion.Header>View Comments</Accordion.Header>
+//                     <Accordion.Body >
+//                       <Card.Text>
+//                         user: this is a great project but could use some UX
+//                         improvements!
+//                       </Card.Text>
+//                       <Card.Text>user: can I join your team?</Card.Text>
+//                       <Card.Text>user: neato!</Card.Text>
+//                     </Accordion.Body>
+//                   </Accordion.Item>
+//                 </Accordion>
 
-                <Card.Footer className="text-muted">
-                  Project Created: github_n00b  |  <a href="https://github.com/Jason-M-Bailey/nbaplayersearch">
-                    Visit Github Repo
-                  </a>
-                </Card.Footer>
-              </Card.Body>
-            </Card>
-          </div>
-        </Row>
-      </div>
-    </section>
-  );
-}
+//                 <Card.Footer className="text-muted">
+//                   Project Created: github_n00b  |  <a href="https://github.com/Jason-M-Bailey/nbaplayersearch">
+//                     Visit Github Repo
+//                   </a>
+//                 </Card.Footer>
+//               </Card.Body>
+//             </Card>
+//           </div>
+//         </Row>
+//       </div>
+//     </section>
+//   );
+// }
 
-export default ProjectCard;
+// export default ProjectCard;
