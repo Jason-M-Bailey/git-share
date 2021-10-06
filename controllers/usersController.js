adfcb8e54729bc146076703c534d9c6280e2fb3f
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-10-05 23:18:48 -0400

    added methods for user registration and login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,8 @@
 const db = require("../models");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
 module.exports = {
     findAll: function(req, res) {
@@ -19,5 +23,40 @@ module.exports = {
             .create(req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(400).json(err));
+    },
+    findOne: function(req, res) {
+        db.User
+            .findOne({ email: req.body.email }, async (err, doc) => {
+                if (err) throw err;
+                if (doc) res.send("User Already Exists");
+                if (!doc) {
+                  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+            
+                  const newUser = new User({
+                    email: req.body.email,
+                    password: hashedPassword,
+                  });
+                  await newUser.save();
+                  res.send("User Created");
+                }
+            });
+    },
+    authenticate: function(req, res, next) {
+        /* passport.authenticate("local", (err, user, info) => {
+            if (err) throw err;
+            if (!user) {
+                res.send("No user exists");
+            } else {
+                req.logIn(user, (err) => {
+                    if (err) throw err;
+                    res.send("Successfully Authenticated");
+                    console.log(req.user);
+                });
+            }
+        }) (req, res, next); */
+        res.json({success: true});
+    },
+    thisUser: function(req, res) {
+        res.send(req.user);
     }
 };
