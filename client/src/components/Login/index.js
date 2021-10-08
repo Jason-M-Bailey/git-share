51722df9a37c7e69267348ec2bf135ec285288fe
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-10-08 17:01:51 -0400

    resolved merge issues

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Swal from "sweetalert2";
 import { Wrapper } from "./Login.styles";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Swal from "sweetalert2";
+
 
 function Login() {
   const [loginUsername, setLoginUsername] = useState("");
@@ -11,9 +12,6 @@ function Login() {
 
   const login = (event) => {
     event.preventDefault();
-
-
-    // THURSDAY BREAKAGE
     console.log({
       username: loginUsername,
       password: loginPassword,
@@ -21,46 +19,27 @@ function Login() {
 
     const user = { username: loginUsername, password: loginPassword}
 
-    // axios
-      // .post("/api/users/login", user)
-      // .then((res) => {
-        // console.log("this is")
-        // console.log(res.data);
-        // console.log(JSON.stringify(res.data))
-        // localStorage.setItem('gs-user', JSON.stringify(res.data));
-        // window.location.href = "/";
-    // ^^^ THURSDAY BREAKAGE
-
     if (!loginUsername || !loginPassword) {
       Swal.fire({
         icon: "error",
         title: "Please",
         text: "Please Enter Information",
       });
-    } else {
-      console.log({
-        username: loginUsername,
-        password: loginPassword,
-
-      });
-
-      axios
-        .post("/api/users/login", {
-          username: loginUsername,
-          password: loginPassword,
-        })
-        .then((res) => {
-          console.log(res);
-          window.location.href = "/";
-        })
-        .catch((err) => {
-          Swal.fire({
-            icon: "error",
-            title: "Please",
-            text: "Please Enter Correct Login",
-          });
-        });
     }
+
+    axios
+      .post("/api/users/login", user)
+      .then((res) => {
+        localStorage.setItem('gs-user', JSON.stringify(res.data));
+        window.location.href = "/";
+      })
+      .catch((err) => {
+        Swal.fire({
+          icon: "error",
+          title: "Please",
+          text: "Please Enter Correct Login",
+        });
+      });
   };
 
   // this is only here so the app can continue to be tested
@@ -96,10 +75,7 @@ function Login() {
             </form>
           </Form>
         </div>
-
-        <h5 className="mt-3">
-          <a href="/register">Click here to create an account first</a>
-        </h5>
+        <h5 className="mt-3">New user? <a href="/register">Create an account</a>.</h5>
       </Wrapper>
     </div>
   );
